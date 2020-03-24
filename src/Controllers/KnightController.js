079cc219606a53d29e895e507c03b1e07ba99771
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-03-24 18:45:05 -0300

    Passing knight corner movement tests

--- src/Controllers/KnightController.js
+++ src/Controllers/KnightController.js
@@ -1,24 +1,36 @@
 const KnightController = {};
 
-KnightController.moveList = {
-  upRight: {
-    oneCellY: { x: 2, y: 1 },
-    twoCellsY: { x: 1, y: 2 },
-  },
-  upLeft: {
-    oneCellY: { x: -2, y: 1 },
-    twoCellsY: { x: -1, y: 2 },
-  },
-  downRight: {
-    oneCellY: { x: 2, y: -1 },
-    twoCellsY: { x: 1, y: -2 },
-  },
-  downLeft: {
-    oneCellY: { x: -2, y: -1 },
-    twoCellsY: { x: -1, y: -2 },
-  },
-};
+KnightController.moveList = [
+//   upRight: {
+  /* oneCellY: */ { x: 2, y: 1 },
+  /* twoCellsY: */ { x: 1, y: 2 },
+  //   },
+  //   upLeft: {
+  /* oneCellY: */ { x: -2, y: 1 },
+  /* twoCellsY: */ { x: -1, y: 2 },
+  //   },
+  //   downRight: {
+  /* oneCellY: */ { x: 2, y: -1 },
+  /* twoCellsY: */ { x: 1, y: -2 },
+  //   },
+  // downLeft: {
+  /* oneCellY: */ { x: -2, y: -1 },
+  /* twoCellsY: */ { x: -1, y: -2 },
+  // },
+];
 
-KnightController.getPossibleDestinations = (currentPosition) => [];
+KnightController.getPossibleDestinations = ({ x: currentX, y: currentY }) => {
+  const possibleMoves = [];
+  KnightController.moveList.forEach(({ x, y }) => {
+    const nextX = x + currentX;
+    if (nextX < 0 || nextX > 7) return;
+    const nextY = y + currentY;
+    if (nextY < 0 || nextY > 7) return;
+
+    possibleMoves.push({ x: currentX + x, y: currentY + y });
+  });
+
+  return possibleMoves;
+};
 
 module.exports = KnightController;
